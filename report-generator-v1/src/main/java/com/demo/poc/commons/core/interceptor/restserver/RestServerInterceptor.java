8c2b79d6fd492e37ecb8f2e06d6eaafb7ba16663
Miguel Armas <marmasa@proveedor.intercorp.com.pe>
2025-04-21 06:45:03 -0500

    Update TimeoutLevel

--- report-generator-v1/src/main/java/com/demo/poc/commons/core/interceptor/restserver/RestServerInterceptor.java
+++ report-generator-v1/src/main/java/com/demo/poc/commons/core/interceptor/restserver/RestServerInterceptor.java
@@ -5,13 +5,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
-@Component
 @Slf4j
 @RequiredArgsConstructor
 public class RestServerInterceptor implements WebFilter {
