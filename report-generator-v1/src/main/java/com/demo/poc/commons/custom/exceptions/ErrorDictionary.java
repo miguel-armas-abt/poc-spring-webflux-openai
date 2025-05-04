0c08ef3b9e2983deef9191b7854693c65f157ff5
Miguel <miguel.armas.abt@gmail.com>
2025-05-04 17:55:27 -0500

    Upgrade Core Library

--- report-generator-v1/src/main/java/com/demo/poc/commons/custom/exceptions/ErrorDictionary.java
+++ report-generator-v1/src/main/java/com/demo/poc/commons/custom/exceptions/ErrorDictionary.java
@@ -1,6 +1,5 @@
 package com.demo.poc.commons.custom.exceptions;
 
-import com.demo.poc.commons.core.errors.dto.ErrorType;
 import com.demo.poc.commons.core.errors.exceptions.GenericException;
 import com.demo.poc.commons.core.errors.exceptions.InvalidFieldException;
 import com.demo.poc.commons.core.errors.exceptions.InvalidStreamingData;
@@ -15,8 +14,6 @@ import org.springframework.http.HttpStatus;
 
 import java.util.Arrays;
 
-import static com.demo.poc.commons.core.errors.dto.ErrorType.BUSINESS;
-import static com.demo.poc.commons.core.errors.dto.ErrorType.SYSTEM;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
@@ -25,25 +22,24 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 public enum ErrorDictionary {
 
   //system: 01.00.xx
-  UNEXPECTED_SSL_EXCEPTION("01.00.01", "Unexpected SSL error for HTTP client", SYSTEM, INTERNAL_SERVER_ERROR, UnexpectedSslException.class),
-  INVALID_STREAMING_DATA("01.00.02", "Streaming data is not processable", SYSTEM, INTERNAL_SERVER_ERROR, InvalidStreamingData.class),
-  ERROR_READING_JSON("01.00.03", "Error reading JSON", SYSTEM, INTERNAL_SERVER_ERROR, JsonReadException.class),
+  UNEXPECTED_SSL_EXCEPTION("01.00.01", "Unexpected SSL error for HTTP client", INTERNAL_SERVER_ERROR, UnexpectedSslException.class),
+  INVALID_STREAMING_DATA("01.00.02", "Streaming data is not processable", INTERNAL_SERVER_ERROR, InvalidStreamingData.class),
+  ERROR_READING_JSON("01.00.03", "Error reading JSON", INTERNAL_SERVER_ERROR, JsonReadException.class),
 
   //no such properties and components: 01.01.xx
-  NO_SUCH_REST_CLIENT("01.01.01", "No such rest client", SYSTEM, INTERNAL_SERVER_ERROR, NoSuchRestClientException.class),
-  NO_SUCH_REST_CLIENT_ERROR_EXTRACTOR("01.01.02", "No such rest client error extractor",SYSTEM, INTERNAL_SERVER_ERROR, NoSuchRestClientErrorExtractorException.class),
-  NO_SUCH_PARAM_MAPPER("01.01.03", "No such param mapper", SYSTEM, INTERNAL_SERVER_ERROR, NoSuchParamMapperException.class),
-  NO_SUCH_AREA_TYPE_REPORTER("01.01.04", "No such area type reporter", SYSTEM, BAD_REQUEST, NoSuchAreaTypeReporterException.class),
-  NO_SUCH_CONCLUSION_TYPE_REPORTER("01.01.05", "No such conclusion type reporter", SYSTEM, BAD_REQUEST, NoSuchConclusionTypeReporterException.class),
+  NO_SUCH_REST_CLIENT("01.01.01", "No such rest client", INTERNAL_SERVER_ERROR, NoSuchRestClientException.class),
+  NO_SUCH_REST_CLIENT_ERROR_EXTRACTOR("01.01.02", "No such rest client error extractor",INTERNAL_SERVER_ERROR, NoSuchRestClientErrorExtractorException.class),
+  NO_SUCH_PARAM_MAPPER("01.01.03", "No such param mapper", INTERNAL_SERVER_ERROR, NoSuchParamMapperException.class),
+  NO_SUCH_AREA_TYPE_REPORTER("01.01.04", "No such area type reporter", INTERNAL_SERVER_ERROR, NoSuchAreaTypeReporterException.class),
+  NO_SUCH_CONCLUSION_TYPE_REPORTER("01.01.05", "No such conclusion type reporter", INTERNAL_SERVER_ERROR, NoSuchConclusionTypeReporterException.class),
 
   //business and bad requests: 01.02.xx
-  INVALID_FIELD("01.02.01", "Invalid field", BUSINESS, BAD_REQUEST, InvalidFieldException.class),
-  ERROR_READING_DOCX("01.01.01", "Error reading DOCX", SYSTEM, INTERNAL_SERVER_ERROR, DocxReadException.class),
-  FORM_IMAGE_IS_REQUIRED("01.01.02", "Form image is required", BUSINESS, BAD_REQUEST, FormImageIsRequiredException.class),;
+  INVALID_FIELD("01.02.01", "Invalid field", BAD_REQUEST, InvalidFieldException.class),
+  ERROR_READING_DOCX("01.01.01", "Error reading DOCX", INTERNAL_SERVER_ERROR, DocxReadException.class),
+  FORM_IMAGE_IS_REQUIRED("01.01.02", "Form image is required", BAD_REQUEST, FormImageIsRequiredException.class),;
 
   private final String code;
   private final String message;
-  private final ErrorType type;
   private final HttpStatus httpStatus;
   private final Class<? extends GenericException> exceptionClass;
 
